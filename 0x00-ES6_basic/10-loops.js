f5968aad202c7e7449eaf03143e292fe4114f020
JuggernautTej <tiwatej@gmail.com>
2024-07-26 17:50:38 +0100

    populated task 10 files

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
 /* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+    for (let idx = 0; idx < array.length; idx++) {
+      array[idx] = appendString + array[idx];
+    }
+  
+    return array;
+  }
