d83e215b74e65427c3f73108b77d868cdbc87921
JuggernautTej <tiwatej@gmail.com>
2024-07-29 20:00:38 +0100

    populated task 0 files

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function getListStudents() {
+    const dArray = [ {id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstname: 'James', location: 'Columbia' },
+        { id: 5, firstname: 'Serena', location: 'San Francisco'}
+     ];
+    return dArray; 
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,3 @@
+# 0x03. ES6 Data Manipulation
+
+This houses the projects on Maps, Array, Typed Array, Set Data Structure, Map Data Structure, WeakMap
