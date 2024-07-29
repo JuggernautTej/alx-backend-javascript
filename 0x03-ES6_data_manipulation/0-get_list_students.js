292fcebdc1e2cd0f5070825fbe8b3622d8bcf812
JuggernautTej <tiwatej@gmail.com>
2024-07-29 20:10:22 +0100

    populated task 0 files again

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 export default function getListStudents() {
-    const dArray = [ {id: 1, firstname: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstname: 'James', location: 'Columbia' },
-        { id: 5, firstname: 'Serena', location: 'San Francisco'}
+    return [ {id: 1, firstname: "Guillaume", location: "San Francisco" },
+        { id: 2, firstname: "James", location: "Columbia" },
+        { id: 5, firstname: "Serena", location: "San Francisco"}
      ];
-    return dArray; 
 }
