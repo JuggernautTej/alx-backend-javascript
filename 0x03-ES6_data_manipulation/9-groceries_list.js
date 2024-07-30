3b46531b622379ea4ed09e0bec26ff8334f20196
JuggernautTej <tiwatej@gmail.com>
2024-07-30 17:21:30 +0100

    populated task 9 files

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default function groceriesList(){
+    const shopMap = new Map();
+    shopMap.set('Apples', 10);
+    shopMap.set('Tomatoes', 10);
+    shopMap.set('Pasta', 10);
+    shopMap.set('Rice', 10);
+    shopMap.set('Banana', 5);
+    
+    return shopMap;
+}
