8bbb87085c99c50492a7c611187901f2f597b685
JuggernautTej <tiwatej@gmail.com>
2024-07-30 17:39:27 +0100

    updated task 9 files

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,14 @@
 /* eslint-disable */
 export default function groceriesList(){
+    /*
     const shopMap = new Map();
     shopMap.set('Apples', 10);
     shopMap.set('Tomatoes', 10);
-    shopMap.set('Pasta', 10);
-    shopMap.set('Rice', 10);
+    shopMap.set('Pasta', 1);
+    shopMap.set('Rice', 1);
     shopMap.set('Banana', 5);
     
     return shopMap;
+    */
+   return new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]])
 }
