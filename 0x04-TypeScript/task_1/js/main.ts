d87fa67c170b57fa8408ddd3ccc7e1677c27ccda
JuggernautTej <tiwatej@gmail.com>
2024-07-31 22:39:35 +0100

    completed task 1 file

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,31 @@ function printTeacher(firstName: string, lastName: string): string {
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+}
+
+interface StudentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+class StudentClass {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return `Currently working`;
+    }
+
+    display(): string {
+        return `${this.firstName}`;
+    }
+}
