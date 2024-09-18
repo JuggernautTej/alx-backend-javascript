5d1ea3ba46222d7ec07589f86dd261b3cd6d48ff
JuggernautTej <tiwatej@gmail.com>
2024-09-18 19:45:02 +0100

    updated task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,9 @@
 /*eslint-disable*/
-const { createServer } = require('node:http');
-
-const port = 1245;
-
-const app = createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+const http = require('http');
+const app = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello Holberton School!');
 });
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+app.listen(1245);
 
 module.exports = app;
