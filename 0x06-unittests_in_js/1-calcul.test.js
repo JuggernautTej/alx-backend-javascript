e02a1c1e512cf58258d1f3e5c63f65f218b99612
JuggernautTej <tiwatej@gmail.com>
2024-09-20 21:25:04 +0100

    updated task 1 again

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -6,10 +6,16 @@ describe('Calculate Tests', function (){
         it('should return -8', function (){
             assert.equal(calculateNumber(-1.7, -6.4, 'SUM'), -8);
         });
+        it('should return 15', function (){
+            assert.equal(calculateNumber(11.7, 3.4, 'SUM'), 15);
+        });
     });
     describe('SUBTRACT tests', function(){
         it('should return 3', function (){
             assert.equal(calculateNumber(5.7, 3.4, 'SUBTRACT'), 3);
+        });
+        it('should return -1', function (){
+            assert.equal(calculateNumber(2.6, 3.5, 'SUBTRACT'), -1);
         });
     });
     describe('DIVIDE tests', function(){
